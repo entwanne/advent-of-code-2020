6b80702b57cc3ed2dad5541e6e1d955de70454ba
Antoine Rozo <antoine.rozo@people-doc.com>
2020-12-11 10:26:44 +0100

    D11 - use cache

--- d11.py
+++ d11.py
@@ -9,11 +9,22 @@ adjacents = [
     if (dx, dy) != (0, 0)
 ]
 
-def nb_adjacents_p1(grid, x, y):
+def nb_adjacents_p1(grid, x, y, neighbors={}):
     count = 0
-    for dx, dy in adjacents:
-        tx, ty = x+dx, y+dy
-        if 0 <= ty < len(grid) and 0 <= tx < len(grid[ty]):
+    k = (x, y)
+    nbs = neighbors.get(k, None)
+    if nbs is None:
+        nbs = neighbors[k] = set()
+        for dx, dy in adjacents:
+            tx, ty = x+dx, y+dy
+            if 0 <= ty < len(grid) and 0 <= tx < len(grid[ty]):
+                if grid[ty][tx] == '#':
+                    nbs.add((tx, ty))
+                    count += 1
+                elif grid[ty][tx] == 'L':
+                    nbs.add((tx, ty))
+    else:
+        for tx, ty in nbs:
             if grid[ty][tx] == '#':
                 count += 1
     return count
@@ -43,16 +54,28 @@ def count_seats(grid):
 grid_p1 = process_seats(grid, nb_adjacents_p1, 4)
 print(count_seats(grid_p1))
 
-def nb_adjacents_p2(grid, x, y):
+def nb_adjacents_p2(grid, x, y, neighbors={}):
     count = 0
-    for dx, dy in adjacents:
-        tx, ty = x+dx, y+dy
-        while 0 <= ty < len(grid) and 0 <= tx < len(grid[ty]) and grid[ty][tx] != 'L':
+    k = (x, y)
+    nbs = neighbors.get(k, None)
+    if nbs is None:
+        nbs = neighbors[k] = set()
+        for dx, dy in adjacents:
+            tx, ty = x+dx, y+dy
+            while 0 <= ty < len(grid) and 0 <= tx < len(grid[ty]):
+                if grid[ty][tx] == 'L':
+                    nbs.add((tx, ty))
+                    break
+                if grid[ty][tx] == '#':
+                    nbs.add((tx, ty))
+                    count += 1
+                    break
+                tx += dx
+                ty += dy
+    else:
+        for tx, ty in nbs:
             if grid[ty][tx] == '#':
                 count += 1
-                break
-            tx += dx
-            ty += dy
     return count
 
 grid_p2 = process_seats(grid, nb_adjacents_p2, 5)
