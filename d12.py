6e8c635577b0951c90d9c418706598de0d784f9b
Antoine Rozo <antoine.rozo@gmail.com>
2020-12-12 10:05:06 +0100

    D12 - complex

--- d12.py
+++ d12.py
@@ -3,54 +3,40 @@ import sys
 commands = [(line[0], int(line[1:])) for line in sys.stdin]
 
 directions = {
-    'N': (+0, -1),
-    'E': (+1, +0),
-    'S': (+0, +1),
-    'W': (-1, +0),
+    'N': +0 -1j,
+    'E': +1 +0j,
+    'S': +0 +1j,
+    'W': -1 +0j,
 }
-turns = {
-    'N': {'L': 'W', 'R': 'E'},
-    'E': {'L': 'N', 'R': 'S'},
-    'S': {'L': 'E', 'R': 'W'},
-    'W': {'L': 'S', 'R': 'N'},
-}
-
-# p1
-
-x, y, d = 0, 0, 'E'
-
-for cmd, arg in commands:
-    if cmd == 'F':
-        dx, dy = directions[d]
-        x, y = x + arg * dx, y + arg * dy
-    elif cmd in directions:
-        dx, dy = directions[cmd]
-        x, y = x + arg * dx, y + arg * dy
-    else: # LR
-        for _ in range(arg // 90):
-            d = turns[d][cmd]
-
-print(abs(x) + abs(y))
-
-# p2
-
 angles = {
     'L': -1j,
     'R': +1j,
 }
 
-x, y = 0, 0
-wx, wy = 10, -1
+# p1
+
+z, current_dir = 0, directions['E']
 
 for cmd, arg in commands:
     if cmd == 'F':
-        x, y = x + arg * wx, y + arg * wy
+        z += arg * current_dir
     elif cmd in directions:
-        dx, dy = directions[cmd]
-        wx, wy = wx + arg * dx, wy + arg * dy
+        z += arg * directions[cmd]
     else: # LR
-        wz = wx + wy * 1j
-        wz *= angles[cmd] ** (arg // 90)
-        wx, wy = int(wz.real), int(wz.imag)
+        current_dir *= angles[cmd] ** (arg // 90)
 
-print(abs(x) + abs(y))
+print(int(abs(z.real) + abs(z.imag)))
+
+# p2
+
+z, w = 0, 10-1j
+
+for cmd, arg in commands:
+    if cmd == 'F':
+        z += arg * w
+    elif cmd in directions:
+        w += arg * directions[cmd]
+    else: # LR
+        w *= angles[cmd] ** (arg // 90)
+
+print(int(abs(z.real) + abs(z.imag)))
