9ed75586d80dfaee2a6b5ce2e9cb91a42c1dcf3a
Antoine Rozo <antoine.rozo@people-doc.com>
2020-12-15 13:44:33 +0100

    D15 - use list instead of dict

--- d15.py
+++ d15.py
@@ -4,12 +4,17 @@ numbers = [int(n) for n in sys.stdin.read().split(',')]
 
 
 def find_nth(nth):
-    memory = {n: i for i, n in enumerate(numbers)}
+    memory = [-1] * max(nth, *numbers)
+    for i, n in enumerate(numbers):
+        memory[n] = i
+
     last = numbers[-1]
     a = len(numbers) - 1
 
     for i in range(len(numbers), nth):
-        b = memory.get(last, a)
+        b = memory[last]
+        if b < 0:
+            b = a
         memory[last] = a
         last = a - b
         a = i
